f5f14171b61679f4ed571d6a62bef5712cd4aea5
Édua Péter <petere.23szv@acsjszki.hu>
2024-12-17 11:36:30 +0100

    Login oldal
    javítások, CSS bővítése, új komponentek hozzáadása

--- src/app/_components/item-list/item-list.component.spec.ts
+++ src/app/_components/item-list/item-list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { ItemListComponent } from './item-list.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('ItemListComponent', () => {
+  let component: ItemListComponent;
+  let fixture: ComponentFixture<ItemListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [ItemListComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(ItemListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
