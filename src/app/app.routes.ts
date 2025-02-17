a9a4669c2b72769bac767caf83206f45fbc95171
Édua Péter <petere.23szv@acsjszki.hu>
2025-02-17 18:50:07 +0100

    User Profile
    teszt kép, user adatok, gombok, responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './_components/login/login.component';
 import { StorageManagementComponent } from './_components/storage-management/storage-management.component';
 import { authGuard } from './auth.guard';
 import { NavbarComponent } from './_components/navbar/navbar.component';
+import { ProfileAdminComponent } from './_components/profile-admin/profile-admin.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     {path: 'navbar', component: NavbarComponent},
+    {path: 'profileAdmin', component: ProfileAdminComponent},
     { 
       path: 'dashboard', 
       component: StorageManagementComponent,
