7ea994e44e5cc96ef3c65cdf840c180c8e88edcc
NiedermayerDavid <nididavid2@gmail.com>
2025-04-18 00:08:29 +0200

    JUnit tesztek
    Added JUnit tests:
    -Palletcontrollertest
    -storagecontrollertest

--- raktarproject/src/main/java/com/helixlab/raktarproject/service/PalletService.java
+++ raktarproject/src/main/java/com/helixlab/raktarproject/service/PalletService.java
@@ -6,46 +6,69 @@ import com.helixlab.raktarproject.model.Shelfs;
 import com.helixlab.raktarproject.model.Users;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-
 public class PalletService {
-    
+
     private Pallets layer = new Pallets();
-    
-    
-    public Pallets getPalletsById(Integer id){
+
+    public Pallets getPalletsById(Integer id) {
         return layer.getPalletsById(id);
     }
-    
-    public Boolean deletePalletById(Integer id) {
-        Pallets u = getPalletsById(id);
 
-        if (u != null) {
-            return layer.deletePalletById(id);
-        } else {
-            System.err.println("The user doesn't exist");
+    public Boolean deletePalletById(Integer id) {
+        // Ellenőrizzük, hogy az id létezik-e az adatbázisban
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.helixLab_raktarproject_war_1.0-SNAPSHOTPU");
+        EntityManager em = emf.createEntityManager();
+        try {
+            Pallets pallet = em.createNamedQuery("Pallets.findById", Pallets.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+
+            if (pallet != null) {
+                // Ha az id létezik, töröljük a raklapot
+                return layer.deletePalletById(id);
+            } else {
+                System.err.println("The Pallet with ID " + id + " doesn't exist in the database");
+                return false;
+            }
+        } catch (NoResultException e) {
+            System.err.println("The Pallet with ID " + id + " doesn't exist in the database");
             return false;
+        } catch (Exception e) {
+            System.err.println("Error checking pallet existence: " + e.getLocalizedMessage());
+            return false;
+        } finally {
+            em.clear();
+            em.close();
         }
     }
-    
+
     public void addPalletToShelf(String skuCode, Integer shelfId, Integer height) {
         try {
-            // Validáció: Ellenőrizzük, hogy a SKU létezik-e az items táblában
             EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.helixLab_raktarproject_war_1.0-SNAPSHOTPU");
             EntityManager em = emf.createEntityManager();
 
             try {
+                // Validáció: Ellenőrizzük, hogy a SKU létezik-e az items táblában
                 Query query = em.createQuery("SELECT i FROM Items i WHERE i.sku = :skuCode");
                 query.setParameter("skuCode", skuCode);
                 Items item = (Items) query.getSingleResult();
-
                 if (item == null) {
                     throw new IllegalArgumentException("SKU code '" + skuCode + "' does not exist in the items table");
                 }
+
+                // Validáció: Ellenőrizzük, hogy a shelfId létezik-e a shelfs táblában
+                Query shelfQuery = em.createQuery("SELECT s FROM Shelfs s WHERE s.id = :shelfId");
+                shelfQuery.setParameter("shelfId", shelfId);
+                Shelfs shelf = (Shelfs) shelfQuery.getSingleResult();
+                if (shelf == null) {
+                    throw new IllegalArgumentException("Shelf with id " + shelfId + " does not exist");
+                }
             } catch (javax.persistence.NoResultException e) {
-                throw new IllegalArgumentException("SKU code '" + skuCode + "' does not exist in the items table");
+                throw new IllegalArgumentException("Invalid input: " + e.getMessage());
             } finally {
                 em.close();
             }
@@ -56,7 +79,7 @@ public class PalletService {
             throw new RuntimeException("Service layer error: " + e.getMessage(), e);
         }
     }
-    
+
     public void movePalletBetweenShelfs(Integer palletId, Integer fromShelfId, Integer toShelfId, Integer userId) {
         try {
             // Validáció: Ellenőrizzük, hogy a pallet létezik-e
